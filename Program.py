085a5c306933a1528dda0f37440cbce1125326aa
Rohan <rohanmitra8@gmail.com>
2022-06-24 21:28:31 +0400

    From infix completed

--- Program.py
+++ Program.py
@@ -13,6 +13,7 @@ class BinaryParseTree:
     def __init__(self, root=None): self.root = root
     
     def fromList(self, list_repr):
+        #Assumes Prefix notation
         def makeTree(currNode,listrep):
             if isinstance(listrep,str):
                 return self.Node(listrep)
@@ -28,8 +29,21 @@ class BinaryParseTree:
         
         self.root=makeTree(self.root,list_repr)
     
-    def toList(self): pass # (part I)
-    
+    def toList(self): 
+        #Assumes Prefix notation
+        def convert(currNode):
+            if currNode.left is None and currNode.right is None:
+                return currNode.data
+            
+            res=[currNode.data]
+            if currNode.left:
+                res.append(convert(currNode.left))
+            if currNode.right:
+                res.append(convert(currNode.right))
+            return res                           
+        
+        return convert(self.root)
+        
     def prettyPrint(self): 
         #Copied from: https://stackoverflow.com/questions/34012886/print-binary-tree-level-by-level-in-python
         #To be changed
@@ -37,7 +51,7 @@ class BinaryParseTree:
         def height(root):
             return 1 + max(height(root.left), height(root.right)) if root else -1  
         nlevels = height(root)
-        width =  pow(2,nlevels+1)
+        width =  pow(2,nlevels)
     
         q=[(root,0,width,'c')]
         levels=[]
@@ -62,20 +76,35 @@ class BinaryParseTree:
             for n in l:
                 valstr= str(n[0].data)
                 if n[3]=='r':
-                    linestr+=' '*(n[2]-preline-1-seg-seg//2)+ '¯'*(seg +seg//2)+'\\'
+                    linestr+=' '*(n[2]-preline-1-seg-seg//2)+ ' '*(seg +seg//2)+'\\'
                     preline = n[2] 
                 if n[3]=='l':
-                   linestr+=' '*(n[2]-preline-1)+'/' + '¯'*(seg+seg//2)  
+                   linestr+=' '*(n[2]-preline-1)+'/' + ' '*(seg+seg//2)  
                    preline = n[2] + seg + seg//2
                 pstr+=' '*(n[2]-pre-len(valstr))+valstr #correct the potition acording to the number size
                 pre = n[2]
             print(linestr)
             print(pstr)   
-            
         
         print('-'*95)
     
-    def fromPrefix(self, expr=''): pass # to be implemented
+    def fromPrefix(self, expr=''): 
+        #Expr example: '(/ (- a b) 3)' 
+        #Raise exception for invalid expression TBD
+        operators='/+*-^'
+        if expr[1] not in operators: #First thing inside () should be an operator
+            raise Exception
+        #Convert to an array rep:
+        import re
+        expr=re.sub('\(','[',expr)
+        expr=re.sub('\)',']',expr)
+        expr=re.sub( '([-/^+*]|[0-9]|[a-z])', r'"\1"' ,expr)
+        expr=re.sub(" ",',',expr)
+        arr=eval(expr) #Returns the array in infix notation
+        #Call fromList function with the list representation:
+        self.fromList(arr)
+        return self
+        
     
     def fromInfix(self, expr=''): pass # (part II)
     
@@ -109,6 +138,7 @@ class BinaryParseTree:
 
 class XBinaryParseTree(BinaryParseTree): # to be implemented
     pass # (part III)
+    #Rearrange the expression into infix notation and then call self.fromInfix :)
 
 
 def test1(*args):
@@ -116,17 +146,19 @@ def test1(*args):
         tree=BinaryParseTree()
         tree.fromList(listrep)
         tree.prettyPrint() 
-        #assert tree.toList() == listrep
+        assert tree.toList() == listrep
         
 #Test Q1:
 
-test1(['+', 'a', '1'],['/', ['+', 'x', 'y'], '2'],['*', 'A', ['+', 'B', ['^', 'C', 'D']]])    
+test1(['+', 'a', '1'],
+        ['/', ['+', 'x', 'y'], '2'],
+        ['*', 'A', ['+', 'B', ['^', 'C', 'D']]],
+        ['+',['*',['+','a','b'],['-','a','b']],['^',['+','a','b'],'2']],
+        ['-',['*','2',['*',['^','x','3'],['^','y','3']]],['-',['^','x','2'],['^','y','2']]] )
 
-# test1([['+', 'a', '1'],
-#        ['/', ['+', 'x', 'y'], '2'],
-#        ['*', 'A', ['+', 'B', ['^', 'C', 'D']]],
-#        #Convert these to prefix first:
-#        [(a+b)*(a-b)+(a+b)^2],
-#        [2*x^3*y^3-x^2-y^2]  ])
+
+tree=BinaryParseTree()
+tree.fromPrefix('(/ (- a b) 3)')
+
         
  
